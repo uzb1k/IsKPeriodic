dfa2028f85fcdc6d5f8bd10dc6f7af2749c18cf5
uzb1k <166876731+uzb1k@users.noreply.github.com>
2024-06-08 23:21:02 +0200

    Add files via upload

---
+++ IsKPeriodic.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool IsKPeriodic(string s, int k) {
+	// Проверяем, если k не равно 0
+	if (k > 0) {
+		// Проверяем делимость строки на k
+		if ((s.size() % k) != 0) {
+			return false;
+		}
+		// Основные два цикла для сравнения строки и подстроки
+		for (int i = 0; i < k; i += k) {
+			char base = s[i];
+			for (int j = 0; j < s.size(); j += k) {
+				if (base != s[j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	return false;
+}
+
+int main() {
+
+	std::string s = "abcabcabcabc";
+	int k = 3;
+
+	if (IsKPeriodic(s, k)) {
+		std::cout << s << " является кратной числу " << k << std::endl;
+	}
+	else {
+		std::cout << s << " не является кратной числу " << k << std::endl;
+	}
+
+	return 0;
+}
